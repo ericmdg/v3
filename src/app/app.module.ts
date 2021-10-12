b5bceaea807030ee08ecd2677e4363d488fde615
Eric MDG <rasolofonirina.mg@gmail.com>
2021-10-12 22:20:03 +0300

    Downgrade d'angular-admin-lte, admin-lte-css et bootstrap-css-only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,24 @@ import { AppComponent } from './app.component';
 
 import { adminLteConf } from '../conf';
 import { LayoutModule } from 'angular-admin-lte';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarLeftComponent } from './sidebar/sidebar-left/sidebar-left.component';
+import { SidebarRightComponent } from './sidebar/sidebar-right/sidebar-right.component';
+import { FooterLeftComponent } from './footer/footer-left/footer-left.component';
+import { FooterRightComponent } from './footer/footer-right/footer-right.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FooterLeftComponent,
+    FooterRightComponent,
+    SidebarComponent,
+    SidebarLeftComponent,
+    SidebarRightComponent
   ],
   imports: [
     BrowserModule,
